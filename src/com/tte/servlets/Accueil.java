ba4f3b7f8d693fbf7375f1b74ac60f6d6a01e94f
Deterson <geraud.magne@epita.fr>
2020-05-02 20:30:52 +0200

    database working

--- src/com/tte/servlets/Accueil.java
+++ src/com/tte/servlets/Accueil.java
@@ -6,28 +6,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Accueil extends HttpServlet
 {
-    static int count = 0;
     public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
     {
-        count++;
-        response.setContentType("text/html");
-        response.setCharacterEncoding( "UTF-8" );
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\" />");
-        out.println("<title>Test</title>");
-        out.println("</head>");
-        out.println("<body>");
-        if (count== 1)
-            out.println("<p>1ère connexion</p>");
-        else
-            out.println("<p>" + count + "ème connexion</p>");
-        out.println("</body>");
-        out.println("</html>");
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        List<String> ips = (List<String>)this.getServletContext().getAttribute("ips");
+        if (ips == null)
+            ips = new ArrayList<>();
+        ips.add(ip);
+        request.setAttribute("ips", ips);
+
+        this.getServletContext().setAttribute("ips", ips);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
     }
 }
