2579a50ca37aa768dc9168ef597b544d0c65c95a
Deterson <geraud.magne@epita.fr>
2020-06-04 20:05:10 +0200

    [GIT] force push to remove previously committed database password (c'est mon mot de passe Habbo donc flemme)

---
+++ src/com/TestJDBC.java
@@ -0,0 +1,81 @@
+package com;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestJDBC
+{
+    private List<String> messages = new ArrayList<>();
+
+    public List<String> executerTests( HttpServletRequest request ) {
+        /* Chargement du driver JDBC pour MySQL */
+        try {
+            messages.add( "Chargement du driver..." );
+            Class.forName( "com.mysql.cj.jdbc.Driver" );
+            messages.add( "Driver chargé !" );
+        } catch ( ClassNotFoundException e ) {
+            messages.add( "Erreur lors du chargement : le driver n'a pas été trouvé dans le classpath ! <br/>"
+                    + e.getMessage() );
+        }
+
+        /* Connexion à la base de données */
+        String url = "jdbc:mysql://localhost:3306/to_the_east_dev?serverTimezone=Europe%2FParis";
+        String utilisateur = "totheeast";
+        String motDePasse = System.getenv("DB_PASS");
+        if (motDePasse == null)
+            System.err.println("DB_PASS NON SPÉCIFIÉ DANS L'ENVIRONNEMENT JAVA");
+        Connection connexion = null;
+        Statement statement = null;
+        ResultSet resultat = null;
+        try {
+            messages.add( "Connexion à la base de données..." );
+            connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+            messages.add( "Connexion réussie !" );
+
+            /* Création de l'objet gérant les requêtes */
+            statement = connexion.createStatement();
+            messages.add( "Objet requête créé !" );
+
+            /* Exécution d'une requête de lecture */
+            resultat = statement.executeQuery( "SELECT * FROM country;" );
+            messages.add( "Requête \"SELECT * FROM country\" effectuée !" );
+
+            /* Récupération des données du résultat de la requête de lecture */
+            while ( resultat.next() ) {
+                String name = resultat.getString( "Name" );
+                String continent= resultat.getString( "Continent" );
+                /* Formatage des données pour affichage dans la JSP finale. */
+                messages.add( "Données retournées par la requête : country = " + name + ", continent = " + continent);
+            }
+        } catch ( SQLException e ) {
+            messages.add( "Erreur lors de la connexion : <br/>"
+                    + e.getMessage() );
+        } finally {
+            messages.add( "Fermeture de l'objet ResultSet." );
+            if ( resultat != null ) {
+                try {
+                    resultat.close();
+                } catch ( SQLException ignore ) {
+                }
+            }
+            messages.add( "Fermeture de l'objet Statement." );
+            if ( statement != null ) {
+                try {
+                    statement.close();
+                } catch ( SQLException ignore ) {
+                }
+            }
+            messages.add( "Fermeture de l'objet Connection." );
+            if ( connexion != null ) {
+                try {
+                    connexion.close();
+                } catch ( SQLException ignore ) {
+                }
+            }
+        }
+
+        return messages;
+    }
+}
---
+++ src/com/sdzee/servlets/GestionTestJDBC.java
@@ -0,0 +1,26 @@
+package com.sdzee.servlets;
+
+import com.TestJDBC;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class GestionTestJDBC extends HttpServlet
+{
+    public static final String ATT_MESSAGES = "messages";
+    public static final String VUE          = "/WEB-INF/test_jdbc.jsp";
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        TestJDBC test = new TestJDBC();
+        List<String> messages = test.executerTests(request);
+
+        request.setAttribute(ATT_MESSAGES, messages);
+
+        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    }
+}
